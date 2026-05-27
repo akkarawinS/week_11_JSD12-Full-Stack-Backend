fb68281e2ce0429f6ba95ab06e7ae236741b56ad
AkkarawinS <akkarawinRX@gmail.com>
2026-05-27 14:23:35 +0700

    add helmet and ratelimit

--- src/server.js
+++ src/server.js
@@ -1,56 +1,35 @@
 import express from 'express';
 import { users } from './mockData/Users.js'
 import cors from 'cors'
-import { router as apiRoutes} from './routes/index.js'
+import { router as apiRoutes } from './routes/index.js'
 import { connectDB } from './config/mongodb.js';
-import { connectSupabase} from './config/supabase.js'
+import { connectSupabase } from './config/supabase.js'
 import cookieParser from 'cookie-parser'
+import { limiter } from './middlewares/rateLimiter.js'
+import helmet from 'helmet'
+
+
 const app = express();
 
-const home =`<!doctype html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Express + Tailwind</title>
-      <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="min-h-screen bg-gray-50 text-gray-800">
-      <main class="max-w-2xl mx-auto p-8">
-        <div class="rounded-xl bg-white shadow-sm ring-1 ring-gray-100 p-8">
-          <h1 class="text-3xl font-bold tracking-tight text-blue-600">
-            Hello Client, I am your Server!
-          </h1>
-          <p class="mt-3 text-gray-600">
-            This page is styled with <span class="font-semibold">Tailwind CSS</span> via CDN.
-          </p>
-          <div class="mt-6 flex flex-wrap items-center gap-3">
-            <a href="/users" class="inline-flex items-center rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-              GET /users
-            </a>
-            <span class="text-xs text-gray-500">Try POST/PUT/DELETE with your API client.</span>
-          </div>
-        </div>
-        <footer class="mt-10 text-center text-xs text-gray-400">
-          Express server running with Tailwind via CDN
-        </footer>
-      </main>
-    </body>
-  </html>`
+app.use(helmet());
 
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors(corsOption));
+app.use(limiter);
 
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send(home)
-});
 
 
 app.use('/api', apiRoutes);
 
+app.get('/', (req, res) => {  
+  res.send('Backend API is running')
+});
 
 //Centralized error handling middleware
 app.use((err, req, res, next) => {
